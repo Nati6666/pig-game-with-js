c87b723aff4a522a72e9139b856a68177340e115
Nati Melkamu <natimelkemu@gmail.com>
2025-07-19 08:17:38 -0700

    Implement dice roll game with player switching and score tracking

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const diceI = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-let player0 = document.querySelector('#current--0')
-let player1 = document.querySelector('#current--1')
+let curremt0 = document.querySelector('#current--0')
+let current1 = document.querySelector('#current--1')
+let player0 = document.querySelector('.player--0')
+let player1 = document.querySelector('.player--1')
+
+
 
 
 
@@ -17,7 +21,11 @@ score0.textContent = 0;
 score1.textContent = 0 ;
 diceI.classList.add('hidden')
 
+
+
+const scores = [0,0];
 let currentScore = 0;
+let activeplayer = 0;
 
 
 //rolling funcitonlity 
@@ -34,11 +42,24 @@ btnRoll.addEventListener('click', function(){
     if(diceNum !== 1){
         //if not one im adding the score to teh current user 
         currentScore += diceNum;
-        player0.textContent = currentScore;
-        
 
+        document.getElementById(`current--${activeplayer}`).textContent = currentScore
+    
     }else{
+     
+        
+  
+
         //if one im switching the player 
+        document.getElementById(`current--${activeplayer}`).textContent = 0
+        currentScore =0;
+        activeplayer = activeplayer === 0? 1:0;
+
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+
+
+
 
     }
 })
